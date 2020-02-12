1ddd3263055909a674510cde1e6b51d910d86e93
ILAKIYA S <ilakyaengi@outlook.com>
2020-02-12 18:37:22 +0530

    Add project files.

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ExampleApplication
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+              name: "Example2",
+              url: "Example2",
+              defaults: new { controller = "Example2", action = "Index", id = UrlParameter.Optional }
+          );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Example1", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
